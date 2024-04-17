5e0937f626f62a34c30c78d485f325a660a17310
jaminel <jamineltechnology@gmail.com>
2024-04-17 06:55:38 +0100

    Day8

--- src/Day8.java
+++ src/Day8.java
@@ -11,6 +11,29 @@ public class Day8 {
         phonebook.put(sc.next(),sc.nextInt());
         phonebook.put(sc.next(),sc.nextInt());
         phonebook.put(sc.next(),sc.nextInt());
+        int n= sc.nextInt();
+        sc.nextLine();
+
+
+       while(n>phonebook.size()) {
+           phonebook.put(sc.next(), sc.nextInt());
+       }
+
+
+
+
+        for(int i=0; i<phonebook.size(); i++){
+            String value=sc.next();
+            if(value.isEmpty()){
+                break;
+            }
+            System.out.println(value+"="+phonebook.get(value));
+
+            if(phonebook.get(value)==null){
+                System.out.println("Not found");
+            }
+        }
+
 
     }
 }
