cc3ad0ac085e8f4002f05a928b9f26dc2325b1d2
jaminel <jamineltechnology@gmail.com>
2024-04-17 19:08:37 +0100

    Day8

--- src/Day8Copy.java
+++ src/Day8Copy.java
@@ -7,19 +7,30 @@ public class Day8Copy {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Map<String,Integer> phonenumber= new HashMap<>();
-        int n =0;
+        Map<String, Integer> phonenumber = new HashMap<>();
+        int n = 0;
         sc.nextLine();
 
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             String input = sc.nextLine();
-            String[]split=input.split(" ");
-            String name= split[0];
-            int number= Integer.parseInt(split[1]);
-            phonenumber.put(name,number);
+            String[] split = input.split(" ");
+            String name = split[0];
+            int number = Integer.parseInt(split[1]);
+            phonenumber.put(name, number);
 
+            while (sc.hasNextLine()) {
+                String query = sc.nextLine();
 
+                if (query.isEmpty()) {
+                    break;
+                }
+                if (phonenumber.containsKey(query)) {
+                    System.out.println(query + "=" + phonenumber.get(query));
+                } else {
+                    System.out.println("Not Found");
+                }
 
+            }
         }
     }
 }
